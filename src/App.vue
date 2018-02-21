46826ca7c3ac9e9043ec3c1cd9d5436f1eb69b03
vtroop <vtroop@126.com>
2018-02-21 23:07:35 +0800

    Feat($migration): init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vheader class="header"></vheader>
+    <div class="main">
+      <router-view class="main-wrapper" transition="fade" transition-mode="out-in"></router-view>
+    </div>
+    <vfooter class="footer"></vfooter>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Vheader from './components/Vheader'
+  import Vfooter from './components/Vfooter'
+  // import Store from './vuex/store'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      Vheader,
+      Vfooter
+    },
+    // store: Store
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    font-size: 10px;
+    height:100%;
+  }
+
+  body {
+    -webkit-font-smoothing:antialiased;
+    font-size: 1.6rem;
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, "Helvetica Neue", Arial, "PingFang SC", "Hiragino Sans GB", STHeiti, "Microsoft YaHei", "Microsoft JhengHei", "Source Han Sans SC", "Noto Sans CJK SC", "Source Han Sans CN", "Noto Sans SC", "Source Han Sans TC", "Noto Sans CJK TC", "WenQuanYi Micro Hei", SimSun, sans-serif;
+    color: #404040;
+    line-height: 1.7;
+    height:100%;
+  }
+
+  #wrapper {
+    display: flex;
+    flex-flow: column;
+    height:100%;
+  }
+
+  body a {
+    text-decoration: none;
+  }
+
+  .nav a:link, a:visited {
+    color: inherit;
+  }
+
+  body ul, li, p {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .fade-transition {
+    transition: all .4s ease;
+  }
+
+  .fade-enter, .fade-leave {
+    opacity: 0;
+  }
+
+  .header, .footer {
+    flex: 0 0 auto;
+  }
+
+  .main {
+    flex: 1 0 auto;
+    order: 1;
+    display: flex;
+    justify-content: center;
+  }
+
+  .main-wrapper {
+    width: 80%;
+    max-width: 80rem;
+    transition: all .4s;
+  }
+
+  .footer {
+    order: 3;
+    display: flex;
+    height: 10rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    .main-wrapper {
+      width: 100%;
+    }
+
+    .footer {
+      height: 6rem;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+const routes = [
+  { path: '/home', component: require("./components/Home.vue") },
+  // { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes
+})
+// // 路由map
+// router.map({
+//   '/home': {
+//     component: function (resolve) {
+//       require(['./components/Home'], resolve)
+//     }
+//   },
+//   '/about': {
+//     component: function (resolve) {
+//       require(['./components/About'], resolve)
+//     }
+//   },
+//   '/tags': {
+//     component: function (resolve) {
+//       require(['./components/Tags'], resolve)
+//     }
+//   },
+//   '/article/:id': {
+//     name: 'article',
+//     component: function (resolve) {
+//       require(['./components/Article'], resolve)
+//     }
+//   }
+
+// })
+
+// router.redirect({
+//   '*': '/home'
+// })
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
